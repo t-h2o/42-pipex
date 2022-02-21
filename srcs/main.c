1b1282d717ce7eb8a67fb4b17a488f1c124ffc5f
Theo Grivel <tgrivel@c1r9s7.42lausanne.ch>
2022-02-21 15:53:24 +0100

    make split function

--- srcs/main.c
+++ srcs/main.c
@@ -6,14 +6,45 @@
 /*   By: tgrivel <marvin@42lausanne.ch>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/21 12:24:45 by tgrivel           #+#    #+#             */
-/*   Updated: 2022/02/21 12:24:52 by tgrivel          ###   ########.fr       */
+/*   Updated: 2022/02/21 15:48:07 by tgrivel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"pipex.h"
 
+void	test_split(char *str, char c)
+{
+	printf("\n\tDEBUT TEST\n\n");
+	
+	char	**tab;
+	char	**save;
+	int		i;
+
+	i = -1;
+	while (++i < 30)
+		printf("%d",i%10);
+
+	printf("\n%s\n", str);
+	tab = pp_split(str, c);
+	save = tab;
+	while (*tab)
+	{
+		printf("m ");
+		printf("%s\n", *tab);
+		free(*tab);
+		*tab = 0;
+		tab++;
+	}
+	printf("\n\tFIN  TEST\n");
+	free(save);
+}
+
 int
 	main(void)
 {
-	printf("\n\n\t42\n\n");
+	test_split("I test my split function", ' ');
+	test_split(" I test my split function", ' ');
+	test_split("    I       test my split function", ' ');
+	test_split("I test my split function   ", ' ');
+	test_split(" a  ", ' ');
 }
---
+++ srcs/pp_split.c
@@ -0,0 +1,100 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pp_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tgrivel <tggrivel@student.42lausanne.ch>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/21 12:37:16 by tgrivel           #+#    #+#             */
+/*   Updated: 2022/02/21 15:49:44 by tgrivel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include	"pipex.h"
+
+static int
+	line_counter(char *str, char c)
+{
+	int	r;
+
+	r = 1;
+	if (*str == c)
+		r = 0;
+	while (*str)
+	{
+		if (*str == c)
+		{
+			r++;
+			while (str[1] == c)
+				str++;
+		}
+		str++;
+	}
+	if (*(--str) == c)
+		r--;
+	printf("Il y a %d lignes\n", r);
+	return (r);
+}
+// count the number of separation.
+
+static char
+	*str_cpy(char *str, int s, int e)
+{
+	int		lenght;
+	char	*r;
+
+	lenght = e - s + 1;
+	r = (char *)malloc(lenght + 1);
+	if (!r)
+		return (0);
+	r[lenght] = 0;
+	while (lenght--)
+		r[lenght] = str[s + lenght];
+	return (r);
+}
+/*	start(s), end(e)
+ *	copy from start to end
+ *
+ * 	example :
+ *	("I try split", 0, 0)
+ *	return("I")
+ *
+ *	("I try split", 2, 4)
+ *	end		= 4
+ *	start	= 2
+ *	return("try")
+ */
+
+char
+	**pp_split(char *str, char c)
+{
+	char	**r;
+	int		line;
+	int		start;
+	int		end;
+	int		i;
+
+	line = line_counter(str, c);
+	r = (char **)malloc(sizeof(char *) * line + 1);
+	if (!r)
+		return (0);
+	r[line] = 0;
+	end = 0;
+	i = 0;
+	while (line--)
+	{
+		while (str[end] == c)
+			end ++;
+		start = end;
+		while (str[end] != c)
+			end ++;
+		r[i] = str_cpy(str, start, end - 1);
+		if (!r[i++])
+			return (0);
+	}
+	return (r);
+}
+/*	function split:
+ *	("split_function_by_theo", '_');
+ *	{"split", "function", "by", "theo"}
+ */
