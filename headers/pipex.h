41745083f04fac3fc97b1c8d5c48e57278415afd
Theo Grivel <tgrivel@c1r9s7.42lausanne.ch>
2022-02-21 16:28:48 +0100

    norminette the header

--- headers/pipex.h
+++ headers/pipex.h
@@ -6,15 +6,20 @@
 /*   By: tgrivel <tggrivel@student.42lausanne.ch>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/21 12:41:51 by tgrivel           #+#    #+#             */
-/*   Updated: 2022/02/21 14:13:17 by tgrivel          ###   ########.fr       */
+/*   Updated: 2022/02/21 16:27:56 by tgrivel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PIPEX_H
+# define PIPEX_H
+
 //	Include
 
-#include	<stdio.h>
-#include	<stdlib.h>
+# include	<stdio.h>
+# include	<stdlib.h>
 
 //	pp_split.c
 
 char	**pp_split(char *str, char c);
+
+#endif /* PIPEX_H */
