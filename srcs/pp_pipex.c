efdde28fba95632c07940f96068cfa761a2a5345
Theo Grivel <tgrivel@c1r8s3.42lausanne.ch>
2022-03-03 14:17:11 +0100

    remove child function

--- srcs/pp_pipex.c
+++ srcs/pp_pipex.c
@@ -6,83 +6,69 @@
 /*   By: tgrivel <tggrivel@student.42lausanne.ch>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/24 15:28:05 by tgrivel           #+#    #+#             */
-/*   Updated: 2022/03/01 13:11:42 by tgrivel          ###   ########.fr       */
+/*   Updated: 2022/03/03 14:11:28 by tgrivel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"pipex.h"
-
-static int
-	proc_child(t_cmd *cmd, char **env, pid_t *pid, int *fd)
-{
-	int	ret;
-
-	*pid = fork();
-	if (*pid == -1)
-	{
-		printf("Error, 1st fork\n");
-		return (-1);
-	}
-	if (*pid == 0)
-	{
-		if (dup2(fd[0], STDIN) == -1)
-			return (-1);
-		if (dup2(fd[1], STDOUT) == -1)
-			return (-1);
-		close(fd[0]);
-		close(fd[1]);
-		//printf("excecute %s\n", cmd->cmd);
-		ret = execve(cmd->cmd, cmd->arg, env);
-		if (ret == -1)
-		{
-			printf("Error, execve\n");
-			return (-1);
-		}
-	}
-	return (0);
-}
 
 int
 	pp_pipex(t_info *info, char **env)
 {
 	int		status;
 	int		pipefd[2];
-	int		fildes[2];
-	pid_t	child;
+	pid_t	child[2];
 
 	info->inf.fd = open(info->inf.path, O_RDONLY);
 	info->ouf.fd = open(info->ouf.path, O_CREAT | O_RDWR | O_TRUNC, 0644);
-	if (info->inf.fd < 0 || info->ouf.fd < 0)
-	{
-		printf("Error, open infile/outfile\n");
-		return (-42);
-	}
-	
+
 	if (pipe(pipefd) == -1)
+		printf("Error, fail pipe\n");
+
+	child[0] = fork();
+
+	if (child[0] == -1)
+		printf("Error, fork 1\n");
+
+	if (!child[0])
 	{
-		printf("Error, pipe function\n");
+		if (dup2(info->inf.fd, STDIN) == -1)
+			return (-1);
+		if (dup2(pipefd[0], STDOUT) == -1)
+			return (-1);
+		close(info->inf.fd);
+		close(info->ouf.fd);
+		close(pipefd[1]);
+		execve(info->cmd1.cmd, info->cmd1.arg, env);
+		printf("Error, execve 1\n");
 		return (-1);
 	}
 
+	child[1] = fork();
+
+	if (child[1] == -1)
+		printf("Error, fork 2\n");
+
+	if (!child[1])
+	{
+		if (dup2(pipefd[1], STDIN) == -1)
+			return (-1);
+		//if (dup2(info->ouf.fd, STDOUT) == -1)
+		//	return (-1);
+		close(info->ouf.fd);
+		close(pipefd[0]);
+		execve(info->cmd2.cmd, info->cmd2.arg, env);
+		printf("Error, execve 2\n");
+		return (-1);
+	}
+
+	close(pipefd[0]);
 	close(pipefd[1]);
+	close(info->inf.fd);
+	close(info->ouf.fd);
 
-	fildes[0] = info->inf.fd;
-	fildes[1] = pipefd[0];
-	if (proc_child(&info->cmd1, env, &child, fildes))
-		pp_brexit(info, -4);
-	waitpid(child, &status, 0);
-
-
-	fildes[0] = pipefd[0];
-	fildes[1] = info->ouf.fd;
-	if (proc_child(&info->cmd2, env, &child, fildes))
-		pp_brexit(info, -5);
-	waitpid(child, &status, 0);
-
-	printf("I'm the parent!\n\n");
-
-	printf("\nI waited my kids...\n");
-
+	waitpid(child[0], &status, 0);
+	waitpid(child[1], &status, 0);
 	return (0);
 }
 /*parent
@@ -100,8 +86,6 @@ int
  * │           │execve(cmd, env)
  * │           ▼
  * │
- * │ wait_pid(child1_pid)
- * │ close(infile)
  * │ open (outfile)
  * │ fork()
  * │
@@ -113,8 +97,10 @@ int
  * │           │execve(cmd, env)
  * │           ▼
  * │
- * │wait_pid(child2_pid)
+ * │close(infile)
  * │close(outfile)
+ * │wait_pid(child1_pid)
+ * │wait_pid(child2_pid)
  * │end
  * ▼
  */
